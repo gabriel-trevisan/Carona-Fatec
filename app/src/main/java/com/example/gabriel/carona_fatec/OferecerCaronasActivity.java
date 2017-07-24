ac41c54abb2ef41f90d9adaf0efa667311e18c42
gabriel-trevisan <gabriel.trevisan.santos@gmail.com>
2017-07-23 22:01:12 -0300

    V1 teste funcionalidades Confirmar rota

--- app/src/main/java/com/example/gabriel/carona_fatec/OferecerCaronasActivity.java
+++ app/src/main/java/com/example/gabriel/carona_fatec/OferecerCaronasActivity.java
@@ -1,48 +1,64 @@
 package com.example.gabriel.carona_fatec;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
-import com.google.maps.model.Geometry;
 
 import java.io.IOException;
 
-public class OferecerCaronasActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class OferecerCaronasActivity extends AppCompatActivity {
 
-    private GoogleMap mMap;
+    EditText localizacaoAtual;
+    EditText destino;
+    EditText raio;
+    GeocodingResult[] resultLocalizacaoAtual;
+    GeocodingResult[] resultDestino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oferecer_caronas);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        //Instanciando objetos
+        localizacaoAtual = (EditText) findViewById(R.id.edtLocalizacaoUsuario);
+        destino = (EditText) findViewById(R.id.edtDestinoUsuario);
+        raio = (EditText) findViewById(R.id.edtRaio);
+
+    }
+
+    public void buscarRota(View v){
+
+        String stringLocalizacaoAtual = localizacaoAtual.getText().toString();
+        String stringDestino = destino.getText().toString();
 
         //Key da api de Geo
         String contextGeoApiKey = getResources().getString(R.string.api_key_geo);
 
         GeoApiContext context = new GeoApiContext().setApiKey(contextGeoApiKey);
-        GeocodingResult[] results = new GeocodingResult[0];
+
+        resultLocalizacaoAtual = new GeocodingResult[0];
+        resultDestino = new GeocodingResult[0];
 
         try {
-            results = GeocodingApi.geocode(context,"Rua Manoel Francisco da Silva, Campinas, Brasil").await();
+
+            resultLocalizacaoAtual = GeocodingApi.geocode(context,stringLocalizacaoAtual).await();
+            resultDestino = GeocodingApi.geocode(context,stringDestino).await();
+
+            Intent confirmarRotaActivity = new Intent(this, ConfirmarRotaActivity.class);
+            Bundle extras = new Bundle();
+            extras.putString("resultLocalizacaoAtual", String.valueOf(resultLocalizacaoAtual[0].geometry.location));
+            extras.putString("resultDestino", String.valueOf(resultDestino[0].geometry.location));
+            confirmarRotaActivity.putExtras(extras);
+            startActivity(confirmarRotaActivity);
+
+
         } catch (ApiException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -51,19 +67,6 @@ public class OferecerCaronasActivity extends AppCompatActivity implements OnMapR
             e.printStackTrace();
         }
 
-        System.out.println(results[0].geometry.location);
-
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
     }
 
